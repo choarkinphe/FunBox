f3dcd47013ba3d3730c3a5c4f94d60347e9f20a4
XiaoHua <choarkinphe@outlook.com>
2020-12-31 16:13:53 +0800

    fix FunTips

--- Sources/FunBox/Code/Extension/FunBox+Tips.swift
+++ Sources/FunBox/Code/Extension/FunBox+Tips.swift
@@ -6,29 +6,29 @@
 //
 
 import Foundation
-typealias FunTips = FunBox.Tips
+public typealias FunTips = FunBox.Tips
 extension FunBox {
-    struct Tips {
-        static let tips = FunTips("Tips").localized
-        static let noContactInformation = FunTips("NoContactInformation").localized
-        static let cameraAuthorize = FunTips("CameraAuthorize").localized
-        static let albumSaveAuthorize = FunTips("AlbumSaveAuthorize").localized
-        static let albumAuthorize = FunTips("AlbumAuthorize").localized
-        static let cancel = FunTips("Cancel").localized
-        static let sure = FunTips("Sure").localized
-        static let setting = FunTips("Setting").localized
-        static let loadFailed = FunTips("LoadFailed").localized
-        static let loading = FunTips("Loading").localized
-        static let noData = FunTips("NoData").localized
-        static let repairing = FunTips("Repairing").localized
-        static let loadingException = FunTips("LoadingException").localized
-        static let processing = FunTips("Processing").localized
-        static let saving = FunTips("Saving").localized
-        static let saveSuccess = FunTips("SaveSuccess").localized
-        static let unknow = FunTips("Unknow").localized
+    public struct Tips {
+        public static let tips = FunTips("Tips").localized
+        public static let noContactInformation = FunTips("NoContactInformation").localized
+        public static let cameraAuthorize = FunTips("CameraAuthorize").localized
+        public static let albumSaveAuthorize = FunTips("AlbumSaveAuthorize").localized
+        public static let albumAuthorize = FunTips("AlbumAuthorize").localized
+        public static let cancel = FunTips("Cancel").localized
+        public static let sure = FunTips("Sure").localized
+        public static let setting = FunTips("Setting").localized
+        public static let loadFailed = FunTips("LoadFailed").localized
+        public static let loading = FunTips("Loading").localized
+        public static let noData = FunTips("NoData").localized
+        public static let repairing = FunTips("Repairing").localized
+        public static let loadingException = FunTips("LoadingException").localized
+        public static let processing = FunTips("Processing").localized
+        public static let saving = FunTips("Saving").localized
+        public static let saveSuccess = FunTips("SaveSuccess").localized
+        public static let unknow = FunTips("Unknow").localized
         
-        let localized: String
-        init(_ rawValue: String, bundle: Bundle? = nil) {
+        public let localized: String
+        public init(_ rawValue: String, bundle: Bundle? = nil) {
             localized = rawValue.fb.localized(in: bundle ?? FunBox.bundle)
         }
     }
